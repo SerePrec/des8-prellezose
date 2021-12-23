b449723005aa4667428ecef29124464f304117b8
Sergio E. Prellezo <sereprec@gmail.com>
2021-12-22 21:26:08 -0300

    Cambios de estilos en chat

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -37,15 +37,42 @@ function renderMessages(data) {
   messages.forEach(message => {
     html += `
     <div class="message">
-      <b>${message.user}</b>
-      [ <span style="color:brown;">${new Date(
-        message.timestamp
-      ).toLocaleString()}</span> ] :
-      <i style="color:green;">${message.text}</i>
+      <b style="color:${stringToColour(message.user)};">${message.user}</b>
+      [ <i>${new Date(message.timestamp).toLocaleString()}</i> ] :
+      <span>${message.text}</span>
     </div>
     `;
   });
   return html;
+}
+
+function stringToColour(string) {
+  const colors = [
+    "#e51c23",
+    "#e91e63",
+    "#9c27b0",
+    "#673ab7",
+    "#3f51b5",
+    "#5677fc",
+    "#03a9f4",
+    "#00bcd4",
+    "#009688",
+    "#259b24",
+    "#8bc34a",
+    "#afb42b",
+    "#ff9800",
+    "#ff5722",
+    "#795548",
+    "#607d8b"
+  ];
+  let hash = 0;
+  if (string.length === 0) return hash;
+  for (let i = 0; i < string.length; i++) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    hash = hash & hash;
+  }
+  hash = ((hash % colors.length) + colors.length) % colors.length;
+  return colors[hash];
 }
 
 // Escucha evento del envío del listado de productos
